d3c719f93d9708251c371954163d653f6c69fcf0
hodee <tlsghk8619@gmail.com>
2023-08-24 19:01:05 +0900

    feat: jwt 디코딩 함수, 토큰 유효성 검사 함수 추가

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,39 @@
+import { axiosClient } from './../apis/axios'
+
 export const isValidEmail = (email: string) => {
   return email.includes('@');
 };
 
 export const isValidPassword = (password: string) => {
   return password.length >= 8;
+};
+
+export const jwtDecode = (token: string) => {
+  const base64Url = token.split('.')[1];
+  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  const jsonPayload = decodeURIComponent(
+    window
+      .atob(base64)
+      .split('')
+      .map((c) => `%${`00${c.charCodeAt(0).toString(16)}`.slice(-2)}`)
+      .join(''),
+  );
+
+  return JSON.parse(jsonPayload);
+};
+
+export const isValidToken = (accessToken: string) => {
+  if (!accessToken) {
+    return false;
+  }
+
+  const decodedToken = jwtDecode(accessToken);
+
+  if (!decodedToken.exp || typeof decodedToken.exp !== "number") {
+    return false;
+  }
+
+  const currentTime = Date.now() / 1000;
+
+  return decodedToken.exp > currentTime;
 };
