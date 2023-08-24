7e87cff11307632ad82803f3e457c9e2fb8c326c
hodee <tlsghk8619@gmail.com>
2023-08-24 21:48:54 +0900

    Feat: todoguard 연결

---
+++ src/guards/todo-guard.tsx
@@ -0,0 +1,33 @@
+import { PropsWithChildren, useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuthContext } from '../hooks/useAuthContext';
+
+const TodoGuard = ({ children }: PropsWithChildren) => {
+  const navigate = useNavigate();
+
+  const { authenticated, loading } = useAuthContext();
+  const [checked, setChecked] = useState(false);
+
+  //authentication 체크해주는 함수
+  const checkAuthentication = useCallback(() => {
+    if (!authenticated) {
+      navigate('/signin');
+    } else {
+      setChecked(true);
+    }
+  }, [authenticated]);
+
+  // 페이지에 들어왔을 때 체크바로 해주기
+  useEffect(() => {
+    checkAuthentication();
+  }, []);
+
+  // loading 하는 중이거나 checked를 안한 상태이면 보여주지 않기
+  if (loading || !checked) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export default TodoGuard;
--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,5 +1,5 @@
 export type Task = {
-  id: string;
+  id: number;
   todo: string;
   isCompleted: boolean;
   userId: string;
