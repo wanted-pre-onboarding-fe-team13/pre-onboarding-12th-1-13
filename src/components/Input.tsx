e888163866d9b69d3c04c3bfa6ba11099cecac01
Patata <patattta93@gmail.com>
2023-08-24 17:04:21 +0900

    Feat: 공용 컴포넌트 Input 구현

---
+++ src/components/Input.tsx
@@ -0,0 +1,69 @@
+import { InputHTMLAttributes } from 'react';
+import styled, { DefaultTheme } from 'styled-components';
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  inputSize?: 'small' | 'medium' | 'large';
+  variant?: 'primary' | 'secondary';
+  testId?: string;
+}
+
+const Input = ({ inputSize, variant, testId, ...rest }: InputProps) => {
+  return <StyledInput data-testid={testId} {...rest} />;
+};
+
+export default Input;
+
+const StyledInput = styled.input<InputProps>`
+  padding: 8px 16px;
+  border: none;
+  outline: none;
+  border-radius: 4px;
+  font-size: 16px;
+  transition: border-color 0.3s;
+  &:focus {
+    border-color: ${({ theme }) => theme.color.primary};
+    outline: none;
+  }
+  ${({ inputSize }) => {
+    switch (inputSize) {
+      case 'small':
+        return `
+          font-size: 12px;
+          padding: 4px 8px;
+        `;
+      case 'large':
+        return `
+          font-size: 20px;
+          padding: 12px 24px;
+        `;
+      default:
+        return `
+          font-size: 16px;
+          padding: 8px 16px;
+        `;
+    }
+  }}
+
+  ${({ variant, theme }) => inputVariantStyles({ variant, theme })}
+`;
+
+const inputVariantStyles = ({
+  variant,
+  theme,
+}: {
+  variant?: 'primary' | 'secondary';
+  theme: DefaultTheme;
+}) => {
+  switch (variant) {
+    case 'primary':
+      return `
+        border-color: ${theme.color.primary};
+      `;
+    case 'secondary':
+      return `
+        border-color: ${theme.color.grey200};
+      `;
+    default:
+      return '';
+  }
+};
