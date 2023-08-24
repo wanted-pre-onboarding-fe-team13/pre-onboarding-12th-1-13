ab1f3ad021885d6fbb2efa5742d83f180389e760
angleixx <angielxx94@gmail.com>
2023-08-24 16:20:35 +0900

    Revert "Merge pull request #2 from wanted-pre-onboarding-fe-team13/feature/axios-module"
    This reverts commit 5722352646983d642c7b2c38f85e76a9dc77ce75, reversing
    changes made to 69f7d5fec913f7bc53ad09c1a65461737b1bf7a3.

--- src/apis/todoApi.ts
+++
@@ -1,7 +0,0 @@
-import { AxiosPromise } from 'axios';
-import { authInstance } from './axios';
-import { Task } from '../types/todo';
-
-export const getTodo = async (): AxiosPromise<Task[]> => {
-  return authInstance.get('/todos');
-};
--- src/types/todo.ts
+++
@@ -1,6 +0,0 @@
-export type Task = {
-  id: string;
-  todo: string;
-  isCompleted: boolean;
-  userId: string;
-};
