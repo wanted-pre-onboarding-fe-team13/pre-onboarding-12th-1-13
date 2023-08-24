5c958209907e55e6da3539dae843bbaf7c1c2df2
hodee <tlsghk8619@gmail.com>
2023-08-24 22:54:59 +0900

    Style: Container설정

--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from 'react-router';
 import { UserSignInput } from '../types/type';
 import { isValidEmail, isValidPassword } from '../utils/utils';
 import { useAuthContext } from '../hooks/useAuthContext';
+import { styled } from 'styled-components';
 
 const SignUp = () => {
   const { regist } = useAuthContext();
@@ -27,37 +28,50 @@ const SignUp = () => {
 
   return (
     <div>
-      <div>회원가입</div>
-      <form onSubmit={signUpHandler}>
-        <div>
-          <label>Email</label>
-          <input
-            data-testid="email-input"
-            name="email"
-            placeholder="name@gmail.com"
-            onChange={inputHandler}
-          />
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            data-testid="password-input"
-            name="password"
-            placeholder="••••••••"
-            onChange={inputHandler}
-          />
-        </div>
-        <div></div>
-        <button data-testid="signup-button" disabled={!isValid}>
-          회원가입
-        </button>
-      </form>
-      <p>
-        계정이 있나요? {''}
-        <button onClick={() => navigate('/signin')}>로그인하러 가기</button>
-      </p>
+      <Container>
+        <div>회원가입</div>
+        <form onSubmit={signUpHandler}>
+          <div>
+            <label>Email</label>
+            <input
+              data-testid="email-input"
+              name="email"
+              placeholder="name@gmail.com"
+              onChange={inputHandler}
+            />
+          </div>
+          <div>
+            <label>Password</label>
+            <input
+              data-testid="password-input"
+              name="password"
+              placeholder="••••••••"
+              onChange={inputHandler}
+            />
+          </div>
+          <div></div>
+          <button data-testid="signup-button" disabled={!isValid}>
+            회원가입
+          </button>
+        </form>
+        <p>
+          계정이 있나요? {''}
+          <button onClick={() => navigate('/signin')}>로그인하러 가기</button>
+        </p>
+      </Container>
     </div>
   );
 };
 
 export default SignUp;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 25px;
+  width: 40vw;
+  height: 60vh;
+  min-width: 200px;
+  border: 1px solid black;
+`;
