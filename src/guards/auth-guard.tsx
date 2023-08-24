1f72ec833ec56610c5041526f3a009cc452c4cce
terry <kdjtiger@gmail.com>
2023-08-24 17:07:40 +0900

    Feat: auth-guard form 세팅

--- src/guards/auth-guard.tsx
+++ src/guards/auth-guard.tsx
@@ -1,7 +1,23 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useCallback, useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AuthGuard = ({ children }: PropsWithChildren) => {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  // context 안에 provider 에서 create 한 것 받기
+  const { authenticated } = useContext();
+
+  const checkAuthentication = useCallback(() => {
+    if (authenticated) {
+      navigate('/todo');
+    }
+  }, [authenticated]);
+
+  useEffect(() => {
+    checkAuthentication();
+  }, [checkAuthentication]);
+
+  return <>{children}</>;
 };
 
 export default AuthGuard;
