4787d9910a627db8f606edc19da1804d098a9d38
kkkkkse <eun2abc@gmail.com>
2023-08-25 22:41:36 +0900

    Refactor: todo api 호출 함수 header 제거(axios가 자동으로 추가함)

--- src/apis/todo.ts
+++ src/apis/todo.ts
@@ -2,32 +2,22 @@ import { axiosInstance } from './axios';
 
 export const getTodos = async () => {
   const { data } = await axiosInstance.get('/todos');
+
   return data;
 };
 
 export const createTodo = async (todo: string) => {
-  const { data } = await axiosInstance.post(
-    '/todos',
-    { todo: todo },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    },
-  );
+  const { data } = await axiosInstance.post('/todos', { todo: todo });
+
   return data;
 };
 
 export const updateTodo = async (id: number, todo: string, isCompleted: boolean) => {
-  const { data } = await axiosInstance.put(
-    `/todos/${id}`,
-    { todo: todo, isCompleted: isCompleted },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    },
-  );
+  const { data } = await axiosInstance.put(`/todos/${id}`, {
+    todo: todo,
+    isCompleted: isCompleted,
+  });
+
   return data;
 };
 
