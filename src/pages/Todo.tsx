79982a9103fa4cbeb684156a266abf0e43c24892
angleixx <angielxx94@gmail.com>
2023-08-25 14:08:39 +0900

    Fix: Todo 폴더 임포트 경로 수정

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 import { useState, useMemo } from 'react';
 import { useContextNullCheck } from '../hooks/useContextNullCheck';
-import { TodoList } from '../components/todo/TodoList';
-import { NewTodoForm } from '../components/todo';
+import { TodoList } from '../components/Todo/TodoList';
+import { NewTodoForm } from '../components/Todo';
 import Button from '../components/Button';
 
 export const Todo = () => {
