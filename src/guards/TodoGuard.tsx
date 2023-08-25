23b0bfecb96e6ff3fee845882c21a8a5b19bc48d
kkkkkse <eun2abc@gmail.com>
2023-08-25 23:46:51 +0900

    Rename: rebase 후 임시 삭제한 파일 원복

---
+++ src/guards/TodoGuard.tsx
@@ -0,0 +1,32 @@
+import { PropsWithChildren, useCallback, useEffect, useState } from 'react';
+
+import { useNavigate } from 'react-router-dom';
+
+import { useAuthContext } from '../hooks/useAuthContext';
+
+const TodoGuard = ({ children }: PropsWithChildren) => {
+  const navigate = useNavigate();
+
+  const { authenticated, loading } = useAuthContext();
+  const [checked, setChecked] = useState(false);
+
+  const checkAuthentication = useCallback(() => {
+    if (!authenticated) {
+      navigate('/signin');
+    } else {
+      setChecked(true);
+    }
+  }, [authenticated]);
+
+  useEffect(() => {
+    checkAuthentication();
+  }, []);
+
+  if (loading || !checked) {
+    return null;
+  }
+
+  return <>{children}</>;
+};
+
+export default TodoGuard;
