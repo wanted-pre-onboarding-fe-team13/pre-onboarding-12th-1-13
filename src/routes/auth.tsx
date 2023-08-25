a2af14b3b3af17b56d57b0d1217676f62db0f1e4
hodee <tlsghk8619@gmail.com>
2023-08-25 12:30:51 +0900

    Fix: Authguard, Todoguard 추가 및 파일 이름 수정

--- src/routes/auth.tsx
+++ src/routes/auth.tsx
@@ -1,6 +1,6 @@
 // import { lazy } from 'react';
 import { Outlet } from 'react-router-dom';
-import AuthGuard from '../guards/auth-guard';
+import AuthGuard from '../guards/Authguard';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
